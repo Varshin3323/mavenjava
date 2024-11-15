32cec254085e0c491185c60e790f0e9c27034c27
Varshin@3323 <chvarshini004@gmail.com>
2024-11-15 10:20:32 +0530

    added files

---
+++ src/main/java/app/mavenjava/javaproject/App.java
@@ -0,0 +1,10 @@
+package app.mavenjava.javaproject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello varshini");
+    }
+}
